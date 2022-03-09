7bb02bd68326afb2362713a44dbceb7de4f448ff
Ben Kolligs <benkolligs@gmail.com>
2022-03-09 12:51:48 -0500

    Added in a proper perspective camera with capabilities of mouse orbit

--- vobo/inc/perspective_camera.h
+++ vobo/inc/perspective_camera.h
@@ -7,27 +7,44 @@
 
 namespace vobo {
 
+enum class CameraMode {
+    LOOKAT=0,
+    RELATIVE
+};
+
 class PerspectiveCamera {
    private:
-    glm::vec3 position_             = {0.0, 0.0, 0.0};
     glm::mat4 rotationMatrix_       = glm::mat4(1.0f);
     glm::mat4 viewMatrix_           = glm::mat4(1.0f);
     glm::mat4 projectionMatrix_     = glm::mat4(1.0f);
     glm::mat4 viewProjectionMatrix_ = glm::mat4(1.0f);
-    glm::vec3 upVector_             = {0.0f, 1.0f, 0.0f};
+    glm::vec3 viewPivotPos_           = {0.0f, 0.0f, 0.0f};
+    glm::mat4 viewPivotRot_           = glm::mat4(1.0f);
+    glm::vec3 camPosInPivot_ = {0.0f, 0.0f, 0.0f};
+    glm::vec3 camPosInWorld_ = {0.0f, 0.0f, 0.0f};
+    CameraMode mode_;
 
    public:
-    PerspectiveCamera(float fov, float width, float height);
+    PerspectiveCamera(float fov, float width, float height, CameraMode mode = CameraMode::LOOKAT);
     ~PerspectiveCamera();
     const glm::mat4& getViewProjection() const { return viewProjectionMatrix_; }
-    void setPosition(glm::vec3 position) { position_ = position; }
-    void rotateBy(float angle, glm::vec3 axis) {
+	const glm::vec3& getCamWorld() const {return camPosInWorld_;}
+    const glm::vec3& getCamPivot() const {return camPosInPivot_;}
+    
+    void setCamPivotPos(glm::vec3 &&position) { camPosInPivot_ = position; recalculateViewMatrix();}
+
+    /* Dealing with camera in it's local coordinates */
+    const glm::mat4 & getCamRot() const {return rotationMatrix_;}
+    void rotateCamPivot(float angle, glm::vec3&& axis) {
         rotationMatrix_ = glm::rotate(rotationMatrix_, glm::radians(angle), axis);
         recalculateViewMatrix();
     }
-    void setRotation(float angle, glm::vec3 axis) {
-        rotationMatrix_ = glm::rotate(glm::mat4(1.0f), glm::radians(angle), axis);
-        recalculateViewMatrix();
+    
+	const glm::vec3 & getPivot() const {return viewPivotPos_;}
+    void setPivotPos(glm::vec3 && target) { viewPivotPos_ = target; recalculateViewMatrix();
+    }
+    void rotatePivot(float angle, glm::vec3 &&axis) {
+        viewPivotRot_ = glm::rotate(viewPivotRot_, angle, axis); recalculateViewMatrix();
     }
 
     void recalculateViewMatrix();
--- vobo/src/perspective_camera.cpp
+++ vobo/src/perspective_camera.cpp
@@ -1,19 +1,24 @@
 #include "perspective_camera.h"
+#include "log.h"
 
-namespace vobo
-{
+namespace vobo {
 
-PerspectiveCamera::PerspectiveCamera(float fov, float width, float height)
-    : projectionMatrix_{
-          glm::perspective(glm::radians(fov), (float) width / height, 0.1f, 100.0f)} {}
+PerspectiveCamera::PerspectiveCamera(float fov, float width, float height,
+                                     CameraMode mode)
+    : mode_{mode},
+      projectionMatrix_{glm::perspective(
+          glm::radians(fov), (float)width / height, 0.1f, 100.0f)} {}
 
 PerspectiveCamera::~PerspectiveCamera() {}
 
 void PerspectiveCamera::recalculateViewMatrix() {
-    glm::mat4 transform =
-        glm::translate(glm::mat4(1.0f), position_) * rotationMatrix_;
-	viewMatrix_ = glm::inverse(transform);
-	viewProjectionMatrix_ = projectionMatrix_ * viewMatrix_;
+
+    glm::mat4 camInPivot = glm::translate(glm::mat4(1.0f), camPosInPivot_) * rotationMatrix_;
+    glm::mat4 pivotInWorld = glm::translate(glm::mat4(1.0), viewPivotPos_) * viewPivotRot_;
+    glm::vec4 homogCamWorld = pivotInWorld * glm::vec4(camPosInPivot_, 1.0);
+    camPosInWorld_ = {homogCamWorld.x, homogCamWorld.y, homogCamWorld.z};
+	viewMatrix_ = glm::inverse(pivotInWorld*camInPivot);
+    viewProjectionMatrix_ = projectionMatrix_ * viewMatrix_;
 }
 
-} // namespace vobo
+}  // namespace vobo
