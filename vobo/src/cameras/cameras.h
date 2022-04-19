e0701897b4cccd08c941f4e0c166695319912512
Ben Kolligs <benkolligs@gmail.com>
2022-04-19 13:01:12 -0400

    Added camera inclusive header

---
+++ vobo/src/cameras/cameras.h
@@ -0,0 +1,9 @@
+#ifndef _vobo_cameras_h_
+#define _vobo_cameras_h_
+
+#include "orthographic_camera.h"
+#include "perspective_camera.h"
+#include "perspective_controller.h"
+
+
+#endif //_vobo_cameras_h_ header
